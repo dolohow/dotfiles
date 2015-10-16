39abdd09accd992668ad02e04d598254a74af5f1
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-10-16 14:01:26 +0200

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+set -x TERM          screen-256color
+set -x _JAVA_OPTIONS -Dawt.useSystemAAFontSettings=setting
+set -x ANDROID_HOME  ~/.android/sdk
+set -x GOPATH        ~/go
+
+set -x PATH          ~/.gem/ruby/2.2.0/bin $PATH
+
+alias studia         "cd ~/static/Drive/studia"
+alias nethogs        "nethogs enp4s0"
+
+source               .servers
+source               .work
