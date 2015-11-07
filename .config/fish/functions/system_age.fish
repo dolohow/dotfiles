73ea9aa08960d64f0af12d619593764f3c4f57a5
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-11-07 13:08:31 +0100

    fish: Add system_age function
    This function can check how "old" is your system.
    Currently works only on Arch Linux

---
+++ .config/fish/functions/system_age.fish
@@ -0,0 +1,14 @@
+function system_age
+  if test ! -e /var/log/pacman.log
+    echo "You can only run this script on Arch Linux"
+    return
+  end
+
+  # Assume installation date for first logged information in pacman.log
+  set installation_date (date -d (head -1 /var/log/pacman.log | cut -d ' ' -f 1,2 | tr -d '[]') +%s)
+  set current_date (date +%s)
+  set count_days (math "( $current_date - $installation_date ) / ( 60 * 60 * 24 )")
+
+  echo "Your system is running $count_days days"
+end
+
