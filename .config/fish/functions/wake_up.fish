2e6ab7c7cb00a6f5d40ed38f838af8461abb9c29
Łukasz Żarnowiecki <dolohow@outlook.com>
2016-05-15 14:00:41 +0200

    wake_up.fish: Improve argument handling

--- .config/fish/functions/wake_up.fish
+++ .config/fish/functions/wake_up.fish
@@ -1,6 +1,10 @@
-function wake_up
+function wake_up --argument-names 'minutes'
+  if test -n $minutes
+    set minutes 26
+    printf "Using default %s minutes sleep time\n" $minutes
+  end
   amixer -c 0 set Master 10 > /dev/null
-  sleep $argv\m
+  sleep $minutes\m
   amixer -c 0 set Master 50 > /dev/null
   sleep 1s
   amixer -c 0 set Master 10 > /dev/null
