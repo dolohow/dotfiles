3cd7f3908840af71ce76aa48dcf36ab4baa9ff1f
Łukasz Żarnowiecki <dolohow@outlook.com>
2016-05-15 13:54:13 +0200

    system_age.fish: Show also in year

--- .config/fish/functions/system_age.fish
+++ .config/fish/functions/system_age.fish
@@ -8,7 +8,8 @@ function system_age
   set installation_date (date -d (head -1 /var/log/pacman.log | cut -d ' ' -f 1,2 | tr -d '[]') +%s)
   set current_date (date +%s)
   set count_days (math "( $current_date - $installation_date ) / ( 60 * 60 * 24 )")
+  set count_years (echo "scale=2; $count_days / 365" | bc)
 
-  echo "Your system is running $count_days days"
+  echo "Your system is running $count_days days which corresponds to $count_years years"
 end
 
