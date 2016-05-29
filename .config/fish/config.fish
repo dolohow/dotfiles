65ac4670910aee78a039f87734049416982b5d67
Łukasz Żarnowiecki <dolohow@outlook.com>
2016-05-29 23:15:34 +0200

    fish: Remove unused stuff from config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,11 @@
 set -x TERM          screen-256color
 set -x _JAVA_OPTIONS -Dawt.useSystemAAFontSettings=setting
 set -x ANDROID_HOME  ~/.android/sdk
-set -x GOPATH        ~/go
 
 set -x PATH          $PATH
 
 set -x SSH_AUTH_SOCK ~/.ssh/agent.sock
 
-alias studia         "cd ~/static/Drive/studia"
 alias nethogs        "nethogs enp4s0"
 
 source               .servers
