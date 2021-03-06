f5934386c2a71758383b3bbbe59ab50205f414a8
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2021-03-06 17:46:28 +0100

    fish: Update changing volume level method

--- .config/fish/functions/wake_up.fish
+++ .config/fish/functions/wake_up.fish
@@ -3,9 +3,9 @@ function wake_up --argument-names 'minutes'
     set minutes 26
     printf "Using default %s minutes sleep time\n" $minutes
   end
-  amixer -c 0 set Master 10 > /dev/null
+  amixer -D pulse sset Master 10% > /dev/null
   sleep $minutes\m
-  amixer -c 0 set Master 50 > /dev/null
+  amixer -D pulse sset Master 50% > /dev/null
   sleep 1s
-  amixer -c 0 set Master 10 > /dev/null
+  amixer -D pulse sset Master 10% > /dev/null
 end
