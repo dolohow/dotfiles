3cad550ee8b0546a7d2d00861e62a27389ac226b
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2025-02-07 13:34:28 +0100

    fish: Drop unused stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,7 @@
 set -x EDITOR        vim
-set -x _JAVA_OPTIONS -Dawt.useSystemAAFontSettings=setting
-set -x ANDROID_HOME  ~/.android/sdk
 
 set -x SSH_AUTH_SOCK ~/.ssh/agent.sock
 
 fish_add_path ~/go/bin
 
-source               .servers
 source               .work
